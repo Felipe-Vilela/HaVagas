9d8b1bd30f476c654a92dd12ee188ef99f6a05cb
Felipe Vilela <felipe.vilela01@outlook.com>
2025-08-30 11:10:01 -0300

    feat: create date picker and pop up

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/havagas/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/havagas/MainActivity.kt
@@ -1,11 +1,13 @@
 package br.edu.ifsp.scl.ads.prdm.sc303898x.havagas
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.scl.ads.prdm.sc303898x.havagas.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private val activityMainBinding: ActivityMainBinding by lazy {
@@ -15,5 +17,44 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
+
+        with(activityMainBinding) {
+            dataNascimentoEt.setOnClickListener {
+                val calendar = Calendar.getInstance()
+                val ano = calendar.get(Calendar.YEAR)
+                val mes = calendar.get(Calendar.MONTH)
+                val dia = calendar.get(Calendar.DAY_OF_MONTH)
+
+                val datePickerDialog = DatePickerDialog(
+                    this@MainActivity,
+                    { _, selectedYear, selectedMonth, selectedDay ->
+                        val dataSelecionada = Calendar.getInstance()
+                        dataSelecionada.set(selectedYear, selectedMonth, selectedDay)
+                        val formatoData = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                        dataNascimentoEt.setText(formatoData.format(dataSelecionada.time))
+                    },
+                    ano, mes, dia
+                )
+                datePickerDialog.show()
+            }
+            salvarBt.setOnClickListener {
+                val dataNascimento = dataNascimentoEt.text.toString()
+
+                val mensagemDoPopup =
+                    "Data de Nascimento: ${dataNascimento.ifEmpty { "Não selecionada" }}"
+
+                val builder = AlertDialog.Builder(this@MainActivity)
+
+                builder.setTitle("Dados do Formulário")
+                builder.setMessage(mensagemDoPopup)
+
+                builder.setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                }
+
+                val dialog = builder.create()
+                dialog.show()
+            }
+        }
     }
 }
