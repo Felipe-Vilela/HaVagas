21c1ddfd68b56d5b2f00aafbb40f134b4f03a526
Felipe Vilela <felipe.vilela01@outlook.com>
2025-08-31 12:39:16 -0300

    feat: add phone and pop up update

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/havagas/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/havagas/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.RadioButton
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -22,6 +23,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(activityMainBinding.root)
 
         with(activityMainBinding) {
+            adicionarCelularCb.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    celularEt.visibility = View.VISIBLE
+                } else {
+                    celularEt.visibility = View.GONE
+                    celularEt.text.clear()
+                }
+            }
+
             dataNascimentoEt.setOnClickListener {
                 val calendar = Calendar.getInstance()
                 val ano = calendar.get(Calendar.YEAR)
@@ -87,35 +97,47 @@ class MainActivity : AppCompatActivity() {
                 val nomeCompleto = nomeEt.text.toString()
                 val email = emailEt.text.toString()
                 val desejaReceberEmails = receberEmailsCb.isChecked
+                val telefoneFixoTexto = telefoneFixoEt.text.toString()
+                val telefoneFixo = tipoTelefoneRg.checkedRadioButtonId
+                val telefoneFixoSelecionado = findViewById<RadioButton>(telefoneFixo).text.toString()
+                val celular = celularEt.text.toString()
+                val sexo = sexoRg.checkedRadioButtonId
+                val sexoSelecionado = findViewById<RadioButton>(sexo).text.toString()
                 val dataNascimento = dataNascimentoEt.text.toString()
-
                 val formacaoSelecionada = formacaoSp.selectedItem.toString()
 
                 val builderMensagem = StringBuilder()
-                builderMensagem.appendLine("Nome Completo: ${nomeCompleto}\n")
-                builderMensagem.appendLine("E-mail: ${email.ifEmpty { "Não informado" }}\n")
-                builderMensagem.appendLine("Deseja receber e-mails: ${if (desejaReceberEmails) "Sim" else "Não"}\n")
-                builderMensagem.appendLine("Data de Nascimento: ${dataNascimento.ifEmpty { "Não selecionada" }}\n")
-                builderMensagem.appendLine("Formação: $formacaoSelecionada\n")
+                builderMensagem.appendLine("Nome Completo: ${nomeCompleto}")
+                builderMensagem.appendLine("E-mail: ${email.ifEmpty { "Não informado" }}")
+                builderMensagem.appendLine("Deseja receber e-mails: ${if (desejaReceberEmails) "Sim" else "Não"}")
+                builderMensagem.appendLine("Telefone Fixo: ${telefoneFixoTexto} - ${telefoneFixoSelecionado}")
+
+                if (adicionarCelularCb.isChecked && celular.isNotEmpty()) {
+                    builderMensagem.appendLine("Celular: $celular")
+                }
+
+                builderMensagem.appendLine("Sexo: ${sexoSelecionado}")
+                builderMensagem.appendLine("Data de Nascimento: ${dataNascimento.ifEmpty { "Não selecionada" }}")
+                builderMensagem.appendLine("Formação: $formacaoSelecionada")
 
                 when (formacaoSp.selectedItemPosition) {
                     0, 1 -> {
-                        builderMensagem.appendLine("Ano de Conclusão: ${anoEt.text.ifEmpty { "Não informado" }}\n")
+                        builderMensagem.appendLine("Ano de Conclusão: ${anoEt.text.ifEmpty { "Não informado" }}")
                     }
                     2, 3 -> {
-                        builderMensagem.appendLine("Ano de Conclusão: ${anoEt.text.ifEmpty { "Não informado" }}\n")
-                        builderMensagem.appendLine("Instituição: ${instituicaoEt.text.ifEmpty { "Não informado" }}\n")
+                        builderMensagem.appendLine("Ano de Conclusão: ${anoEt.text.ifEmpty { "Não informado" }}")
+                        builderMensagem.appendLine("Instituição: ${instituicaoEt.text.ifEmpty { "Não informado" }}")
                     }
                     4, 5 -> {
-                        builderMensagem.appendLine("Ano de Conclusão: ${anoEt.text.ifEmpty { "Não informado" }}\n")
-                        builderMensagem.appendLine("Instituição: ${instituicaoEt.text.ifEmpty { "Não informado" }}\n")
-                        builderMensagem.appendLine("Título da Monografia: ${tituloMonografiaEt.text.ifEmpty { "Não informado" }}\n")
-                        builderMensagem.appendLine("Orientador: ${orientadorEt.text.ifEmpty { "Não informado" }}\n")
+                        builderMensagem.appendLine("Ano de Conclusão: ${anoEt.text.ifEmpty { "Não informado" }}")
+                        builderMensagem.appendLine("Instituição: ${instituicaoEt.text.ifEmpty { "Não informado" }}")
+                        builderMensagem.appendLine("Título da Monografia: ${tituloMonografiaEt.text.ifEmpty { "Não informado" }}")
+                        builderMensagem.appendLine("Orientador: ${orientadorEt.text.ifEmpty { "Não informado" }}")
                     }
                 }
 
                 val dialog = AlertDialog.Builder(this@MainActivity)
-                    .setTitle("Dados do Formulário\n")
+                    .setTitle("Dados do Formulário")
                     .setMessage(builderMensagem.toString())
                     .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
                     .create()
