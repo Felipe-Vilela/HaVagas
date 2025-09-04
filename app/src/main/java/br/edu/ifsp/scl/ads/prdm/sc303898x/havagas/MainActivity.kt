a21fdac7fa3b39140214aa5b295e42bf610ddfe2
Felipe Vilela <felipe.vilela01@outlook.com>
2025-09-04 00:13:58 -0300

    gardening: removendo importação desnecessária

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/havagas/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/havagas/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.RadioButton
-import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.ads.prdm.sc303898x.havagas.databinding.ActivityMainBinding
